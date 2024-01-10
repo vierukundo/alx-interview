6fb2a21ec3798e2aafba2e583426a654fce846a2
vierukundo <vierukundo@gmail.com>
2024-01-10 11:14:21 -0800

    Method that determines if all the boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    visited_boxes = []
+    keys = [0]
+
+    while len(visited_boxes) < len(boxes) and len(keys) >= 1:
+        new_keys = []
+        for key in keys:
+            if key not in visited_boxes and 0 <= key < len(boxes):
+                new_keys += boxes[key]
+                visited_boxes.append(key)
+        keys = new_keys
+    if len(visited_boxes) != len(boxes):
+        return False
+    else:
+        return True
