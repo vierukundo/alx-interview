5c5efa54f5e4e652c2c416a79978f4deeff73266
vierukundo <vierukundo@gmail.com>
2024-01-30 11:22:08 -0800

    method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,8 +3,16 @@
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    
     for num in data:
-        if num > 127 or num < 0:
-            return False
+        # Consider only the 8 least significant bits
+        num &= 0xFF
+        
+        # Check if the leading bit is 0 (1-byte character)
+        if num >> 7 == 0b0:
+            continue
+        else:
+            return False  # Invalid leading byte
+    
     return True
