fc15d9d0a0d4b0670eb1f38ceedd44ebd8c92626
vierukundo <vierukundo@gmail.com>
2024-03-14 16:18:25 -0700

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,29 +4,44 @@
 """
 
 
-def determine_winner(n, rounds):
+def determine_winner(n):
     """Return the player with more wins"""
+    if n < 2:
+        return 'Ben'
     players = {'Maria': 0, 'Ben': 0}
-    for _ in range(rounds):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False  # 0 and 1 are not primes
-        for p in range(2, n + 1):
-            if primes[p]:
-                if p * p <= n:
-                    for i in range(p * p, n + 1, p):
-                        primes[i] = False
-                if players['Maria'] == players['Ben']:
-                    players['Maria'] += 1
-                else:
-                    players['Ben'] += 1
-    return 'Maria' if players['Maria'] > players['Ben'] else 'Ben'
+    prime = [True for i in range(n+1)]
+    p = 2
+    player = 'Maria'
+    while (p <= n):
+        if prime[p]:
+            if player == 'Maria':
+                players['Maria'] += 1
+                last_player = player
+                player = 'Ben'
+            else:
+                players['Ben'] += 1
+                last_player = player
+                player = 'Maria'
+            prime[p] = False
+
+            # Update all multiples of p
+            if p * p <= n:
+                for i in range(p * p, n+1, p):
+                    prime[i] = False
+        p += 1
+    if players['Maria'] > players['Ben']:
+        return 'Maria'
+    elif players['Ben'] > players['Maria']:
+        return 'Ben'
+    else:
+        return last_player  # last played player
 
 
 def isWinner(x, nums):
     """Returns the winner of the game"""
     players = {'Maria': 0, 'Ben': 0}
-    for n in nums:
-        winner_of_round = determine_winner(n, x)
+    for i in range(x):
+        winner_of_round = determine_winner(nums[i])
         players[winner_of_round] += 1
     if players['Maria'] == players['Ben']:
         return None  # Winner cannot be determined
